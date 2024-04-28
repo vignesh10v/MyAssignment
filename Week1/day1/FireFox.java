aec21864ec50eb89752d70a9ba3078067a6a774e
vignesh10v <168334155+vignesh10v@users.noreply.github.com>
2024-04-28 14:38:49 +0530

    Add files via upload
    day1

---
+++ Week1/day1/FireFox.java
@@ -0,0 +1,14 @@
+package Week1.day1;
+
+public class FireFox {
+	
+	public static void main(String[] args) {
+		float browserversion = 100.2f;
+		String browserName ="FireFox";
+		boolean isVisible= true;
+		int releaseYear =1998;
+		char browserLogo = 'f';
+		System.out.println(browserversion+"\n"+browserName+"\n"+isVisible+"\n"+releaseYear+"\n"+browserLogo);
+	}
+
+}
---
+++ Week1/day1/HelloTestLeaf.java
@@ -0,0 +1,9 @@
+package Week1.day1;
+
+public class HelloTestLeaf {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome To TestLeaf");
+	}
+
+}
